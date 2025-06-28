c3bfe130cec5d6ced4e6da3905fe69659a50c41f
Igor Malytsky <igor.malytsky@gmail.com>
2025-06-28 13:37:52 +0300

    [Feature] Maintenance
    **Changelog**
    - Upgraded libs
    - Opted-in experimental kotlin time

--- toolkit/time/src/commonMain/kotlin/io/baselines/toolkit/time/InstantExt.kt
+++ toolkit/time/src/commonMain/kotlin/io/baselines/toolkit/time/InstantExt.kt
@@ -1,7 +1,7 @@
 package io.baselines.toolkit.time
 
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
+import kotlin.time.Clock
+import kotlin.time.Instant
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
